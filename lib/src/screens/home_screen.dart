1dfc43031b72bd4725bcd0d7146d998dc50aab0d
Apoorva Aditya <apoorvaufo3@gmail.com>
2021-10-12 16:54:51 -0500

    Added bottom navbar switching

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -9,65 +9,79 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  List<Widget> pages = [
+    Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(color: Colors.blue),
+        FloatingSearchBar(
+          hint: 'Search...',
+          scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+          transitionDuration: const Duration(milliseconds: 800),
+          transitionCurve: Curves.easeInOut,
+          physics: const BouncingScrollPhysics(),
+          axisAlignment: 0.0,
+          openAxisAlignment: 0.0,
+          width: 600,
+          debounceDelay: const Duration(milliseconds: 500),
+          onQueryChanged: (query) {
+            // Call your model, bloc, controller here.
+          },
+          // Specify a custom transition to be used for
+          // animating between opened and closed stated.
+          transition: CircularFloatingSearchBarTransition(),
+          actions: [
+            FloatingSearchBarAction(
+              showIfOpened: false,
+              child: CircularButton(
+                icon: const Icon(Icons.place),
+                onPressed: () {},
+              ),
+            ),
+            FloatingSearchBarAction.searchToClear(
+              showIfClosed: false,
+            ),
+          ],
+          builder: (context, transition) {
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Material(
+                color: Colors.white,
+                elevation: 4.0,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: Colors.accents.map((color) {
+                    return Container(height: 112, color: color);
+                  }).toList(),
+                ),
+              ),
+            );
+          },
+        ),
+      ],
+    ),
+    Container(
+      color: Colors.red,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Container(color: Colors.blue),
-          FloatingSearchBar(
-            hint: 'Search...',
-            scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
-            transitionDuration: const Duration(milliseconds: 800),
-            transitionCurve: Curves.easeInOut,
-            physics: const BouncingScrollPhysics(),
-            axisAlignment: 0.0,
-            openAxisAlignment: 0.0,
-            width: 600,
-            debounceDelay: const Duration(milliseconds: 500),
-            onQueryChanged: (query) {
-              // Call your model, bloc, controller here.
-            },
-            // Specify a custom transition to be used for
-            // animating between opened and closed stated.
-            transition: CircularFloatingSearchBarTransition(),
-            actions: [
-              FloatingSearchBarAction(
-                showIfOpened: false,
-                child: CircularButton(
-                  icon: const Icon(Icons.place),
-                  onPressed: () {},
-                ),
-              ),
-              FloatingSearchBarAction.searchToClear(
-                showIfClosed: false,
-              ),
-            ],
-            builder: (context, transition) {
-              return ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Material(
-                  color: Colors.white,
-                  elevation: 4.0,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: Colors.accents.map((color) {
-                      return Container(height: 112, color: color);
-                    }).toList(),
-                  ),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
+      body: pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(icon: Icon(Icons.more), label: 'More'),
         ],
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
     );
   }
