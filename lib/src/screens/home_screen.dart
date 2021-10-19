f3e0cb21d2b8b7053f5cff2e01e7d794410603bf
Apoorva Aditya <apoorvaufo3@gmail.com>
2021-10-19 16:45:22 -0500

    added google_maps_flutter package

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,11 +13,11 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _currentIndex = 0;
-  List<Widget> pages = [
+  late List<Widget> pages = [
     Stack(
       fit: StackFit.expand,
       children: [
-        Container(color: Colors.blue),
+        MapWidget(),
         FloatingSearchBar(
           hint: 'Search...',
           scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
@@ -83,6 +86,38 @@ class _HomeScreenState extends State<HomeScreen> {
           });
         },
       ),
+    );
+  }
+}
+
+class MapWidget extends StatefulWidget {
+  const MapWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MapWidget> createState() => _MapWidgetState();
+}
+
+class _MapWidgetState extends State<MapWidget> {
+  late Completer<GoogleMapController> _controller = Completer();
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+  static const CameraPosition _kLake = CameraPosition(
+    bearing: 192.8334901395799,
+    target: LatLng(37.43296265331129, -122.08832357078792),
+    tilt: 59.440717697143555,
+    zoom: 19.151926040649414,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      mapType: MapType.hybrid,
+      initialCameraPosition: _kGooglePlex,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
     );
   }
 }
