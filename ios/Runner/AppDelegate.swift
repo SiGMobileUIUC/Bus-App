39d070d3681d452a154b6d72c7115d91af2c6519
Apoorva Aditya <apoorvaufo3@gmail.com>
2021-10-26 16:23:56 -0500

    Added google maps api key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("EMPTY")
+    GMSServices.provideAPIKey("EAIzaSyD1F3cj3qpzkt8hVMyBwzcNyHdwFDsIkwYMPTY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
