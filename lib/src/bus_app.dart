ef6ff1f3911e9774c664e0e379171db36ebfc89b
Apoorva Aditya <apoorvaufo3@gmail.com>
2021-10-26 16:37:55 -0500

    Added provider package and initial api service provider

--- lib/src/bus_app.dart
+++ lib/src/bus_app.dart
@@ -1,3 +1,6 @@
+import 'services/api_service.dart';
+import 'package:provider/provider.dart';
+
 import 'misc/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -28,52 +31,57 @@ class _BusAppState extends State<BusApp> {
       barBackgroundColor: AppColors.secondaryUofILight,
       primaryColor: AppColors.secondaryUofILightest,
     );
-    return PlatformProvider(
-      settings: PlatformSettingsData(iosUsesMaterialWidgets: true),
-      builder: (context) => PlatformApp(
-        debugShowCheckedModeBanner: false,
-        localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
-          DefaultMaterialLocalizations.delegate,
-          DefaultWidgetsLocalizations.delegate,
-          DefaultCupertinoLocalizations.delegate,
-        ],
-        title: "Bus App",
-        home: const HomeScreen(),
-        material: (_, __) => MaterialAppData(
-          theme: ThemeData(
-            brightness: Brightness.light,
-            scaffoldBackgroundColor: Colors.white,
-            canvasColor: Colors.white,
-            inputDecorationTheme: const InputDecorationTheme(
-              hintStyle: TextStyle(color: Colors.white),
+    return MultiProvider(
+      providers: [
+        Provider(create: (_) => ApiService()),
+      ],
+      child: PlatformProvider(
+        settings: PlatformSettingsData(iosUsesMaterialWidgets: true),
+        builder: (context) => PlatformApp(
+          debugShowCheckedModeBanner: false,
+          localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+            DefaultMaterialLocalizations.delegate,
+            DefaultWidgetsLocalizations.delegate,
+            DefaultCupertinoLocalizations.delegate,
+          ],
+          title: "Bus App",
+          home: const HomeScreen(),
+          material: (_, __) => MaterialAppData(
+            theme: ThemeData(
+              brightness: Brightness.light,
+              scaffoldBackgroundColor: Colors.white,
+              canvasColor: Colors.white,
+              inputDecorationTheme: const InputDecorationTheme(
+                hintStyle: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Colors.white,
+              colorScheme: const ColorScheme.light(),
+              appBarTheme: const AppBarTheme(
+                backgroundColor: AppColors.secondaryUofILight,
+                actionsIconTheme: IconThemeData(color: Colors.white),
+                iconTheme: IconThemeData(color: Colors.white),
+              ),
             ),
-            backgroundColor: Colors.white,
-            colorScheme: const ColorScheme.light(),
-            appBarTheme: const AppBarTheme(
-              backgroundColor: AppColors.secondaryUofILight,
-              actionsIconTheme: IconThemeData(color: Colors.white),
-              iconTheme: IconThemeData(color: Colors.white),
+            darkTheme: ThemeData(
+              brightness: Brightness.dark,
+              scaffoldBackgroundColor: Colors.black,
+              backgroundColor: Colors.black,
+              colorScheme: const ColorScheme.dark(),
+              canvasColor: Colors.black,
+              inputDecorationTheme: const InputDecorationTheme(
+                hintStyle: TextStyle(color: Colors.white),
+              ),
+              appBarTheme: const AppBarTheme(
+                backgroundColor: AppColors.primaryUofI,
+                actionsIconTheme: IconThemeData(color: Colors.white),
+                iconTheme: IconThemeData(color: Colors.white),
+              ),
             ),
+            themeMode: ThemeMode.system,
           ),
-          darkTheme: ThemeData(
-            brightness: Brightness.dark,
-            scaffoldBackgroundColor: Colors.black,
-            backgroundColor: Colors.black,
-            colorScheme: const ColorScheme.dark(),
-            canvasColor: Colors.black,
-            inputDecorationTheme: const InputDecorationTheme(
-              hintStyle: TextStyle(color: Colors.white),
-            ),
-            appBarTheme: const AppBarTheme(
-              backgroundColor: AppColors.primaryUofI,
-              actionsIconTheme: IconThemeData(color: Colors.white),
-              iconTheme: IconThemeData(color: Colors.white),
-            ),
+          cupertino: (_, __) => CupertinoAppData(
+            theme: MediaQuery.of(context).platformBrightness == Brightness.light ? lightTheme : darkTheme,
           ),
-          themeMode: ThemeMode.system,
-        ),
-        cupertino: (_, __) => CupertinoAppData(
-          theme: MediaQuery.of(context).platformBrightness == Brightness.light ? lightTheme : darkTheme,
         ),
       ),
     );
