99e046a39b1820461caad720ad39e67745b8b0a4
Apoorva Aditya <apoorvaufo3@gmail.com>
2021-10-26 18:10:50 -0500

    created getStop function and classes

--- lib/src/services/api_service.dart
+++ lib/src/services/api_service.dart
@@ -1,3 +1,4 @@
+import '../models/stop.dart';
 import 'package:dio/dio.dart';
 
 class ApiService {
@@ -8,5 +9,18 @@ class ApiService {
     _dio = Dio(
       BaseOptions(baseUrl: 'https://developer.cumtd.com/api/v2.2/json/'),
     );
+  }
+
+  Future<List<Stop>> getStop(List<String> stopIds) async {
+    Response res = await _dio.get(
+      'getStop',
+      queryParameters: {
+        'key': _apiKey,
+        'stop_id': stopIds.reduce((value, element) => value + ';' + element),
+      },
+    );
+    final List<Map<String, dynamic>> stopsJson =
+        ((res.data as Map<String, dynamic>)['stops'] as List).map((e) => (e as Map<String, dynamic>)).toList();
+    return stopsJson.map((e) => Stop.fromJson(e)).toList();
   }
 }
